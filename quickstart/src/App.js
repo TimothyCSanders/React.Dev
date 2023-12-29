32d1ace1c0bdb05f183257a09335780bc9caffee
Tim Sanders <timothycsanders@gmail.com>
2023-12-28 19:31:07 -0500

    component level and global level count state

--- quickstart/src/App.js
+++ quickstart/src/App.js
@@ -2,37 +2,44 @@ import ShoppingList from './ShoppingList.js';
 import { useState, useEffect } from 'react';
 
 
-function MyButton({needsAlert}) {
-  const [count, setCount] = useState(0);
+function MyButton({appCount, needsAlert, setCount}) {
+  const [buttonCount, setButtonCount] = useState(0);
 
   function handleClick() {
-    setCount(count + 1);
-    
+    setButtonCount(buttonCount + 1);
+    setCount();
   }
 
   useEffect(() => {
-    //setCount(count + 1); // This will always use latest value of count
-    if ((count > 0) && needsAlert)
-      alert('You clicked me ' + count.toString() + ' times !');
-  }, [count]);
+    //setButtonCount(buttonCount + 1); // This will always use latest value of buttonCount
+    if ((buttonCount > 0) && needsAlert)
+      alert('You clicked me ' + buttonCount.toString() + ' times !');
+  }, [buttonCount]);
 
 
 
   return (
     <div>
-      <button onClick={handleClick}>
-        I'm a button. Clicked {count} times.
+      <button onClick={handleClick} >
+        I'm a button. Clicked {buttonCount} times. App count = {appCount}
       </button>
     </div>
   );
 }
 
 export default function MyApp() {
+
+  const [appCount, setAppCount] = useState(0);
+
+  function handleGlobalClick() {
+    setAppCount(appCount + 1);
+  }
+
   return (
     <div>
       <h1>Welcome to my app!!</h1>
-      <MyButton needsAlert={true} />
-      <MyButton needsAlert={false} />
+      <MyButton appCount={appCount} needsAlert={true} setCount={handleGlobalClick} />
+      <MyButton appCount={appCount} needsAlert={false} setCount={handleGlobalClick}  />
       <ShoppingList />
     </div>
   );
